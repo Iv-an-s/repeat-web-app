ce67b975c400902cded88359d2fb5111df341429
Ivan <ivan_semenov@bk.ru>
2023-01-29 16:21:06 +0300

    front: pagination

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -15,7 +15,24 @@ create table products
 insert into products (title, price, category_id)
 values ('Bread', 25, 1),
         ('Milk', 80, 1),
-        ('Cheese', 450, 1),
-        ('Cheese', 550, 1),
-        ('Cheese', 650, 1);
+        ('Cheese1', 450, 1),
+        ('Cheese2', 550, 1),
+        ('Cheese3', 650, 1),
+        ('Cheese4', 450, 1),
+        ('Cheese5', 550, 1),
+        ('Cheese6', 650, 1),
+        ('Cheese7', 750, 1),
+        ('Cheese8', 850, 1),
+        ('Cheese9', 950, 1),
+        ('Cheese10', 1050, 1),
+        ('Cheese11', 1150, 1),
+        ('Cheese12', 1250, 1),
+        ('Cheese13', 1350, 1),
+        ('Cheese14', 1450, 1),
+        ('Cheese15', 1550, 1),
+        ('Cheese16', 1650, 1),
+        ('Cheese17', 1750, 1),
+        ('Cheese18', 1650, 1),
+        ('Cheese19', 1750, 1),
+        ('Cheese20', 1850, 1);
 
--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -1,10 +1,12 @@
 angular.module('market-front', []).controller('indexController', function ($scope, $http) {
-    const contextPath = 'http://localhost:8189/market/'
+    const contextPath = 'http://localhost:8189/market/api/v1/';
+    let currentPageIndex = 1;
 
 //    var loadProducts = function(){ - можно присвоить функцию обычной переменной, и вызывать ее здесь по имени,
 // если в html она не нужна. Используем для вспомагательных функций, чтобы не захламлять $scope
 
     $scope.loadProducts = function(pageIndex = 1){  // запись означает, что если мы не подали аргумент в pageIndex, по дефолту его значение будет равно 1
+        currentPageIndex = pageIndex;
         $http({
             url: contextPath + 'products',
             method: 'GET',
@@ -14,6 +16,7 @@ angular.module('market-front', []).controller('indexController', function ($scop
         }).then(function (response){
             console.log(response)
             $scope.productsPage = response.data;
+            $scope.paginationArray = $scope.generatePagesIndexes(1, $scope.productsPage.totalPages);
         });
     };
 
@@ -25,6 +28,14 @@ angular.module('market-front', []).controller('indexController', function ($scop
 //            });
 //    };
 
+    $scope.generatePagesIndexes = function (startPage, endPage){
+        let arr = [];
+        for(let i = startPage; i < endPage +1; i++){
+            arr.push(i);
+        }
+        return arr;
+    }
+
     $scope.showInfo = function (product){
         alert(product.title);
     }
@@ -32,7 +43,7 @@ angular.module('market-front', []).controller('indexController', function ($scop
     $scope.createNewProduct = function(){
         $http.post(contextPath + 'products', $scope.new_product)
             .then(function successCallback(response){
-                $scope.loadProducts();
+                $scope.loadProducts(currentPageIndex);
                 $scope.new_product = null;
                 }, function failureCallback(response){
                 alert(response.data.message);
@@ -52,5 +63,5 @@ angular.module('market-front', []).controller('indexController', function ($scop
     //     });
     // }
 
-    $scope.loadProducts(2);
+    $scope.loadProducts();
 });
